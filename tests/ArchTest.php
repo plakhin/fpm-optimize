e2471a841c37ef131bd47c4cc36d4c35d23ba30f
Stanislav Plakhin <plakhin@gmail.com>
2025-02-19 21:14:28 +0500

    add `security` preset to the `ArchTest`

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -3,4 +3,5 @@
 use Plakhin\FpmOptimize\Commands\SuggestFpmConfigValues;
 
 arch()->preset()->php();
+arch()->preset()->security();
 arch()->preset()->strict()->ignoring(SuggestFpmConfigValues::class);
